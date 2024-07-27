3025b370abdca6b5155b53929e84c162f3a923bb
young <syoh0313@yonsei.ac.kr>
2024-07-27 15:56:21 +0900

    feat : 결제, payment 정보 받기

--- src/components/Service/ServiceDone.vue
+++ src/components/Service/ServiceDone.vue
@@ -1,33 +1,15 @@
 <template>
   <div class="service-done-container">
     <div class="done-img">
-      <img src="@/assets/check.png" alt="check" width="55">
+      <img src="@/assets/check.png" alt="check" width="75">
     </div>
     <div class="done-bold" id="done-title">주문 완료되었습니다 !</div>
-    <div class="done-content">안내된 계좌로 입금을 완료하셔야 서비스 이용이 가능합니다.<br>
-    설문은 입금 확인과 빠른 검수 후 패널들에게 전달됩니다.</div>
-    <div class="option-border"></div>
-    <div class="done-block">
-      <div class="done-bold" :style="{ flex: 1 }">총 결제금액</div>
-      <div class="done-bold" :style="{ flex: 1.5 }">{{this.totalPrice}} 원</div>
-    </div>
-    <div class="option-border"></div>
-    <div class="done-block">
-      <div class="done-bold" :style="{ flex: 1 }">입금자</div>
-      <div class="done-bold" :style="{ flex: 1.5 }">{{this.$store.state.surveyOption.accountName}}</div>
-    </div>
-    <div class="option-border"></div>
-    <div class="done-block">
-      <div class="done-bold" :style="{ flex: 1 }">계좌번호</div>
-      <div class="done-bold" :style="{ flex: 1.5 }">카카오뱅크 3333-11-5235460<br>
-      (송다예)</div>
-    </div>
-    <div class="option-border"></div>
+    
+    
     <div class="done-content">설문조사의 진행 상황은 마이 페이지에서 확인하실 수 있습니다.<br>
     문의 사항은 카카오채널 @surveasy로 보내주세요.</div>
     <div>
       <router-link to="/mypage" class="pay-btn">마이페이지 가기</router-link>
-      <!-- <button class="pay-btn">마이페이지 가기</button> -->
     </div>
   </div>
 </template>
@@ -35,12 +17,21 @@
 <script>
 import store from '@/store'
 export default {
-  data() {
-    return{
-      totalPrice : store.state.surveyOption.price - (store.state.surveyOption.coupon + store.state.surveyOption.point),
-      accountName : store.state.surveyOption.accountName
-    }
+  mounted(){
+    this.getPaymentKey()
   },
+
+  methods : {
+    getPaymentKey(){
+      const urlParams = new URLSearchParams(window.location.search);
+      const paymentKey = urlParams.get("paymentKey");
+      const orderId = urlParams.get("orderId");
+      const amount = urlParams.get("amount");
+      // 서버에 POST 
+      console.log(paymentKey, orderId, amount)
+
+    }
+  }
 }
 </script>
 
@@ -49,40 +40,29 @@ export default {
   display: flex;
   flex-direction: column;
   font-family: 'Noto Sans KR', sans-serif;
-  margin: 30px;
-}
-.service-done-container .option-border {
-  width: 70%;
-  margin: auto;
-}
-.done-block{
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-left: 20%;
-  margin-top: 30px;
-  margin-bottom: 30px;
+  padding: 120px;
 }
 .done-content {
   margin: auto;
   color: gray;
   font-size: 0.8rem;
   font-weight: 500;
-  margin-bottom: 25px;
-  margin-top: 25px;
+  margin-bottom: 50px;
+  margin-top: 30px;
 }
 .done-bold{
   text-align: left;
   font-size: 1.1rem;
   color: black;
   font-weight: 600;
+  margin-top: 50px;
 }
 #done-title{
   text-align: center !important;
   margin-top: 20px;
 }
 .pay-btn{
-  margin-top: 10%;
+  margin: 50px;
   width: 60%;
   padding: 10px 12px;
   color:#0AAB00;
--- src/views/Service/ServicePayment.vue
+++ src/views/Service/ServicePayment.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ServiceDone />
+    <ServicePay />
 
   </div>
 </template>
