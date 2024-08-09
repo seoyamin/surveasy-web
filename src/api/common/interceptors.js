83cd040f47808346b881ecfaef745e3f43c9e0a1
young <syoh0313@yonsei.ac.kr>
2024-08-09 14:47:53 +0900

    feat : interceptor 개선

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -1,3 +1,12 @@
+import axios from "axios";
+import VueCookies from "vue-cookies"
+import router from "@/router";
+
+function handleTokenExpiration() {
+  VueCookies.remove("refresh_token");
+  localStorage.removeItem("access_token");
+  router.push({ name: 'Login' });
+}
 
 export function setInterceptors(instance) {
   instance.interceptors.request.use(
@@ -14,7 +23,36 @@ export function setInterceptors(instance) {
     function(response) {
       return response;
     },
-    function(error) {
+    async function(error) {
+      const originalRequest = error.config
+      console.log(originalRequest)
+      if(error.response.status == 401 &&  !originalRequest._retry) {
+        originalRequest._retry = true;
+        const refreshToken = VueCookies.get("refresh_token")
+        if(refreshToken == null) {
+          localStorage.removeItem("access_token")
+          router.push({ name: 'Login' })
+          return Promise.reject(error)
+        }
+        try {
+          const response = await axios.post("https://surveasy.store/user/reissue",{
+            refreshToken : refreshToken
+          })
+          if(response.status == 200){
+            localStorage.setItem("access_token", response.data.accessToken)
+            VueCookies.set("refresh_token", response.data.refreshToken)
+            originalRequest.headers.Authorization = 'Bearer ' + response.data.accessToken
+            
+            return instance(originalRequest)
+          }else{
+            handleTokenExpiration()
+            return Promise.reject(error)
+          }
+        } catch (error){
+          handleTokenExpiration()
+          return Promise.reject(error)
+        }
+      }
       return Promise.reject(error);
     },
   );
